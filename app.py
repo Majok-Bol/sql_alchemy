f00ec355a07e95a0cf5e61ac389d1237607fbf42
Majok-Bol <bolmajok001@gmail.com>
2026-06-14 13:24:15 +0300

    add post table

--- app.py
+++ app.py
@@ -14,12 +14,21 @@ class User(db.Model):
     id=db.Column(db.Integer,primary_key=True)
     username=db.Column(db.String(20))
     email=db.Column(db.String(100))
+    posts=db.relationship("Post",back_populates="author")
     def __repr__(self):
         return f"<User id={self.id},username={self.username},email={self.email}>"
-
+class Post(db.Model):
+    id=db.Column(db.Integer,primary_key=True)
+    title=db.Column(db.String(50))
+    body=db.Column(db.String(120))
+    user_id=db.Column(db.Integer,db.ForeignKey("user.id"))
+    author=db.relationship("User",back_populates="posts")
+    def __repr__(self):
+        return f"<Post id={self.id},user_id={self.user_id},title={self.title},body={self.body},author={self.author.username if self.author else None}>"
 @app.shell_context_processor
 def make_shell_contect():
     return {
         "db":db,
-        "User":User
+        "User":User,
+        "Post":Post
     }
